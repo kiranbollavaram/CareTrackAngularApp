cc13dc45080c8f57a1281970c13a8049ebb277e8
kiranbollavaram <41304518+kiranbollavaram@users.noreply.github.com>
2018-07-17 20:15:08 +0530

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -112,13 +112,13 @@ IF EXIST "%DEPLOYMENT_SOURCE%\.angular-cli.json" (
  :: 4. Copy web.config
   IF EXIST "%DEPLOYMENT_SOURCE%\web.config" (
       pushd "%DEPLOYMENT_SOURCE%"
-      call :ExecuteCmd cp web.config
+      call :ExecuteCmd cp web.config dist\
       IF !ERRORLEVEL! NEQ 0 goto error
       popd
 )
 :: 5. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
