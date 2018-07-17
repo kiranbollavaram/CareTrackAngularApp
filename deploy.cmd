977e31b52867e53b98981632fd750002132f74a2
kiranbollavaram <41304518+kiranbollavaram@users.noreply.github.com>
2018-07-17 21:00:46 +0530

    updated deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -93,32 +93,32 @@ echo Handling node.js deployment.
 call :SelectNodeVersion
 
 :: 2. Install npm packages
-IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
-  pushd "%DEPLOYMENT_SOURCE%"
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
  
 :: 3. Angular Prod build
-IF EXIST "%DEPLOYMENT_SOURCE%\.angular.json" (
-   echo Building App in %DEPLOYMENT_SOURCE%...
-   pushd "%DEPLOYMENT_SOURCE%"
+IF EXIST "%DEPLOYMENT_TARGET%\.angular.json" (
+   echo Building App in %DEPLOYMENT_TARGET%...
+   pushd "%DEPLOYMENT_TARGET%"
       call :ExecuteCmd "!NPM_CMD!" run build
       IF !ERRORLEVEL! NEQ 0 goto error
       popd
 )
 
  :: 4. Copy web.config
-  IF EXIST "%DEPLOYMENT_SOURCE%\web.config" (
-      pushd "%DEPLOYMENT_SOURCE%"
+  IF EXIST "%DEPLOYMENT_TARGET%\web.config" (
+      pushd "%DEPLOYMENT_TARGET%"
       call :ExecuteCmd cp web.config dist\
       IF !ERRORLEVEL! NEQ 0 goto error
       popd
 )
 :: 5. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TARGET%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
