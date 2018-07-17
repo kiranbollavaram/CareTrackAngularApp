da20b515b61cfcb6e341b1df85b4a703f87edf2a
kiranbollavaram <41304518+kiranbollavaram@users.noreply.github.com>
2018-07-18 00:38:18 +0530

    Add files via upload

--- deploy.cmd
+++ deploy.cmd
@@ -112,7 +112,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%\.angular.json" (
  :: 4. Copy web.config
   IF EXIST "%DEPLOYMENT_SOURCE%\web.config" (
       pushd "%DEPLOYMENT_SOURCE%"
-      call :ExecuteCmd cp web.config dist\CareTrack\
+      call :ExecuteCmd cp web.config dist\
       IF !ERRORLEVEL! NEQ 0 goto error
       popd
 )
