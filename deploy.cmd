137495f63623e4d89fcd7b2278683ecbcdfe8177
kiranbollavaram <41304518+kiranbollavaram@users.noreply.github.com>
2018-09-10 18:49:14 +0530

    Add files via upload

--- deploy.cmd
+++ deploy.cmd
@@ -88,7 +88,6 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-
 :: 1. Select node version
 call :SelectNodeVersion
 
@@ -99,7 +98,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
- 
+
 :: 3. Angular Prod build
 IF EXIST "%DEPLOYMENT_SOURCE%\.angular.json" (
    echo Building App in %DEPLOYMENT_SOURCE%...
@@ -109,7 +108,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%\.angular.json" (
       popd
 )
 
- :: 4. Copy web.config
+:: 4. Copy web.config
   IF EXIST "%DEPLOYMENT_SOURCE%\web.config" (
       pushd "%DEPLOYMENT_SOURCE%"
       call :ExecuteCmd cp web.config dist\
@@ -121,6 +120,7 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
+
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
